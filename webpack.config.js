1c72569822e114da653e7b22dfc5589db35c7377
Drake Leung <drakeleung42@gmail.com>
2017-08-11 16:43:48 +0800

    + webpack

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+  app: path.join(__dirname, 'src'),
+  dist: path.join(__dirname, 'dist'),
+};
+
+const commonConfig = {
+  entry: {
+    app: PATHS.app,
+  },
+  output: {
+    path: PATHS.dist,
+    filename: '[name].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Mine',
+    }),
+  ],
+}
+
+const developmentConfig = () => {
+  const config = {
+    devServer: {
+      historyApiFallback: true,
+
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // 0.0.0.0 is available to all network devices
+      host: process.env.HOST, // Defaults to `localhost`
+      port: process.env.PORT, // Defaults to 8080
+    },
+  }
+
+  return Object.assign(
+    {},
+    commonConfig,
+    config
+  )
+}
+
+const productionConfig = () => commonConfig
+
+module.exports = (env) => {
+  if (env === 'production') {
+    return productionConfig()
+  }
+
+  return developmentConfig()
+}
